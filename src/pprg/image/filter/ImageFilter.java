8fd62bd7a8470976231a1cf878bae0bfba4fe87a
Andreas Burger <se15m020@technikum-wien.at>
2016-03-26 17:11:43 +0100

    parallel with chunks

--- src/pprg/image/filter/ImageFilter.java
+++ src/pprg/image/filter/ImageFilter.java
@@ -3,11 +3,15 @@ package pprg.image.filter;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 import javax.imageio.ImageIO;
 
 public class ImageFilter {
 
+	private static final int NUM_THREADS = 5;
 	private static TimeLogger t = new TimeLogger();
 
 	public static void main(String[] args) throws IOException {
@@ -15,75 +19,80 @@ public class ImageFilter {
 		t.start();
 		BufferedImage img = ImageIO.read(new File("original.png"));
 		t.end("Read image");
-		BufferedImage output = new BufferedImage(img.getWidth(), img.getHeight(), BufferedImage.TYPE_INT_ARGB);
-
-		traceColor(img, 2, 2);
-		traceColor(img, 2, 3);
-		traceColor(img, 2, 4);
-		traceColor(img, 3, 2);
-		traceColor(img, 3, 3);
-		traceColor(img, 3, 4);
-		traceColor(img, 4, 2);
-		traceColor(img, 4, 3);
-		traceColor(img, 4, 4);
 
 		t.start();
-		applyFilter(img, output, FilterGenerator.sharpeningFilter());
+		int[][][] array = convertToArray(img, img.getWidth(), img.getHeight());
+		t.end("Convert to array");
+
+		t.start();
+		applyFilter(array, FilterGenerator.smoothingFilter(9));
 		t.end("Apply ITF");
 
-		traceColor(output, 3, 3);
+		t.start();
+		BufferedImage output = convertToImage(array, img.getWidth(), img.getHeight());
+		t.end("Convert to image");
 
 		t.start();
 		ImageIO.write(output, "png", new File("output.png"));
 		t.end("Write image");
-
-		// ScheduledExecutorService executor =
-		// Executors.newScheduledThreadPool(256);
-		// Runnable task = () -> {
-		// String threadName = Thread.currentThread().getName();
-		// System.out.println("Hello " + threadName);
-		// };
-		//
-		// executor.scheduleWithFixedDelay(task, 0, 1, TimeUnit.SECONDS);
 	}
 
-	private static void applyFilter(BufferedImage image, BufferedImage output, double[][] filter) {
+	private static void applyFilter(int[][][] image, double[][] filter) {
 		int offset = (filter.length - 1) / 2;
 
-		// loop over all pixels (except border)
-		for (int y = offset; y < image.getHeight() - offset; y++) {
-			for (int x = offset; x < image.getWidth() - offset; x++) {
+		ExecutorService executor = Executors.newFixedThreadPool(NUM_THREADS);
 
-				// loop over pixels for 1 mask
-				int[][] pixels = new int[filter.length][filter[0].length];
-				int i = 0, j = 0;
-				for (int z = -offset; z <= offset; z++) {
-					for (int a = -offset; a <= offset; a++) {
-						pixels[i][j++] = image.getRGB(x + z, y + a);
+		// generate chunks on x axis and start NUM_THREADS
+		for (int chunk = offset; chunk < image.length - offset; chunk += NUM_THREADS) {
+
+			// loop over all pixels (except border)
+			final int chunkk = chunk;
+			Runnable task = () -> {
+				for (int x = chunkk; x < (chunkk + NUM_THREADS) && (x < image.length - offset); x++) {
+					for (int y = offset; y < image[0].length - offset; y++) {
+
+						// loop over pixels for 1 mask
+						final int xx = x;
+						final int yy = y;
+						int[][][] pixels = new int[filter.length][filter[0].length][4];
+						int i = 0, j = 0;
+						for (int z = -offset; z <= offset; z++) {
+							for (int a = -offset; a <= offset; a++) {
+								pixels[i][j++] = image[xx + z][yy + a];
+							}
+							i++;
+							j = 0;
+						}
+
+						// apply filter with found pixels
+						image[xx][yy] = applyFilterOnPixel(pixels, filter);
 					}
-					i++;
-					j = 0;
 				}
+			};
+			executor.execute(task);
+		}
 
-				// apply filter with found pixels
-				output.setRGB(x, y, applyFilter(pixels, filter));
-			}
+		executor.shutdown();
+		try {
+			executor.awaitTermination(Long.MAX_VALUE, TimeUnit.SECONDS);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
 		}
 	}
 
-	private static int applyFilter(int[][] pixels, double[][] filter) {
+	private static int[] applyFilterOnPixel(int[][][] pixels, double[][] filter) {
 		double alpha = 0, red = 0, green = 0, blue = 0;
 		for (int i = 0; i < pixels.length; i++) {
 			for (int j = 0; j < pixels.length; j++) {
-				alpha = getAlpha(pixels[i][j]);
-				red += getRed(pixels[i][j]) * filter[i][j];
-				green += getGreen(pixels[i][j]) * filter[i][j];
-				blue += getBlue(pixels[i][j]) * filter[i][j];
+				red += pixels[i][j][0] * filter[i][j];
+				green += pixels[i][j][1] * filter[i][j];
+				blue += pixels[i][j][2] * filter[i][j];
+				alpha = pixels[i][j][3];
 			}
 		}
 
-		return getColor((int) Math.round(alpha), (int) Math.round(red), (int) Math.round(green),
-				(int) Math.round(blue));
+		return new int[] { (int) Math.round(red), (int) Math.round(green), (int) Math.round(blue),
+				(int) Math.round(alpha) };
 	}
 
 	private static int getAlpha(int color) {
@@ -111,4 +120,36 @@ public class ImageFilter {
 		System.out.printf("pixel (%4d,%4d) - alpha: %3d, red: %3d, green: %3d, blue: %3d\n", x, y, getAlpha(color),
 				getRed(color), getGreen(color), getBlue(color));
 	}
+
+	private static int[][][] convertToArray(BufferedImage image, int width, int height) {
+		int[][][] result = new int[width][height][4];
+
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++) {
+				int pixel = image.getRGB(x, y);
+				int alpha = (pixel >> 24) & 0xff;
+				int red = (pixel >> 16) & 0xff;
+				int green = (pixel >> 8) & 0xff;
+				int blue = (pixel) & 0xff;
+				result[x][y][0] = red;
+				result[x][y][1] = green;
+				result[x][y][2] = blue;
+				result[x][y][3] = alpha;
+			}
+		}
+
+		return result;
+	}
+
+	private static BufferedImage convertToImage(int[][][] array, int width, int height) {
+		BufferedImage ret = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++) {
+				int color = getColor(array[x][y][3], array[x][y][0], array[x][y][1], array[x][y][2]);
+				ret.setRGB(x, y, color);
+			}
+		}
+		return ret;
+	}
+
 }
