2dbe2fb0ab3830ae385c81779b350ba8a8f02a56
Johanna Strutzenberger <johanna.strutzenberger@bildkopie.com>
2016-04-05 21:28:21 +0200

    Added Sharpening filter, Laplacian of Gaussian filter, Sobel filter for horizontal and vertical edges

--- src/pprg/image/filter/FilterGenerator.java
+++ src/pprg/image/filter/FilterGenerator.java
@@ -21,19 +21,40 @@ public class FilterGenerator {
 							  , { 0d, 0d, 0d } };
 	}
 
-	public static double[][] detectEdgesFilter() {
-		return new double[][] { { -1d,  0d, 0d,  0d,  0d }
-							  , {  0d, -2d, 0d,  0d,  0d }
-							  , {  0d,  0d, 6d,  0d,  0d }
-							  ,	{  0d,  0d, 0d, -2d,  0d }
-							  , {  0d,  0d, 0d,  0d, -1d } };
+	public static double[][] detectEdgesLoGFilter() {
+		return new double[][] { { 0.0448, 0.0468,  0.0564, 0.0468, 0.0448 }
+		  					  , { 0.0468, 0.3167,  0.7146, 0.3167, 0.0468 }
+		  					  , { 0.0564, 0.7146, -4.9048, 0.7146, 0.0564 }
+		  					  , { 0.0468, 0.3167,  0.7146, 0.3167, 0.0468 }
+		  					  , { 0.0448, 0.0468,  0.0564, 0.0468, 0.0448 } };
 	}
 
-	public static double[][] sharpeningFilter() {
-		return new double[][] { { -1d,  -1d, -1d, -1d, -1d }
-		  					  , { -1d,   2d,  2d,  2d, -1d }		  
-		  					  , { -1d,   2d,  8d,  2d, -1d }		  
-		  					  ,	{ -1d,   2d,  2d,  2d, -1d }		  
-		  					  , { -1d,  -1d, -1d, -1d, -1d } };
+	public static double[][] detectVerticalEdgesSobelFilter() {
+		return new double[][] { {  1d,  2d,  1d }
+		  					  , {  0d,  0d,  0d }		  
+		  					  , { -1d, -2d, -1d } };
+	}
+
+	public static double[][] detectHorizontalEdgesSobelFilter() {
+		return new double[][] { { 1d,  0d, -1d }
+		  					  , { 2d,  0d, -2d }		  
+		  					  , { 1d,  0d, -1d } };
+	}
+
+	public static double[][] sharpeningFilter(int parameter) {
+		if (parameter % 2 != 1) {
+			throw new IllegalArgumentException("Filter has to be odd size");
+		}
+		double[][] filter = new double[parameter][parameter];
+		for (int i = 0; i < filter.length; i++) {
+			for (int j = 0; j < filter[0].length; j++) {
+				if(i == (parameter-1)/2 && j == (parameter-1)/2) {
+					filter[i][j] =  2d - (1d / (parameter * parameter));
+				} else {
+					filter[i][j] =  0d - (1d / (parameter * parameter));
+				}
+			}
+		}
+		return filter;
 	}
 }
